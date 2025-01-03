2b3ead7e38c083e17dac0dc49be555c1ac828ead
John Hornak <hornak0701@gmail.com>
2025-01-03 18:36:38 -0500

    Created a test file and tested the bear encounter

--- porkcombat.py
+++ porkcombat.py
@@ -6,7 +6,7 @@ To aid with enemy creation could be added
 Initial code by coderchameleon
 '''
 
-# we need to get Hammy's health
+# we need to get Hammy's health from pork.py
 import pork
 
 # Called when it is time for Hammy to fight a bear
@@ -21,14 +21,14 @@ def bearEncounterInitializer():
     print("CREATING A TEST FLAG")
     print("USED FOR TESTING ONLY")
     bestCaseScenario = True
-    print("OUR BEST CASE SCENARIO FLAG IS SET TO " + bestCaseScenario)
+    print("OUR BEST CASE SCENARIO FLAG IS SET TO " + str(bestCaseScenario))
     # Fight encounter loop
-    # Currently just cylces through bear abilities
-    for ability in bearAbilities:
-        # If Hammy is alive or it's the best case scenario, the fight continues
-        while(hammyIsAlive() or bestCaseScenario):
+    # If Hammy is alive or it's the best case scenario, the fight continues
+    while(hammyIsAlive() or bestCaseScenario):
+        # Currently just cylces through bear abilities
+        for ability in bearAbilities:
             # Bear uses its ability (printed in the console) and lowers Hammy's health if applicable
-            print("Bear uses " + ability)
+            print("\nBear uses " + ability)
             pork.health -= bearAbilities[ability]
             # If Hammy can die and has died, the function (fight) ends, False is returned in this situation
             if not bestCaseScenario and not hammyIsAlive():
@@ -36,29 +36,34 @@ def bearEncounterInitializer():
                 return False
             # Calls a function to handle the move selected by the player
             hammySelectedAbility = selectHammyAbility(hammyAbilities)
+            print("Hammy uses " + hammySelectedAbility)
             bearHealth -= hammyAbilities[hammySelectedAbility]
             #if the bear has been defeated, the function (fight) ends, True is returned in this situation
             if bearHealth <= 0:
-                print("The bear has been defeated!")
+                print("\nThe bear has been defeated!")
+                # Lazy Fix to bring Hammy back up above 0 health
+                if pork.health < 1:
+                    pork.health = 1
                 return True
-        # If all goes well, the following shouldn't occur
-        print("exception met: ")
-        print("Hammy must be bacon")
-        return False
+    # If all goes well, the following shouldn't occur
+    print("exception met: ")
+    print("devs might be bacon")
+    return False
 
             
 def selectHammyAbility(hammyAbilities):
     # Getting ability selection and using tolower function to normalize input
     print("What do you do?")
     for ability in hammyAbilities:
-        print(ability)
-    hammySelectedAbility = input().lower()
+        print(ability, end=" ")
+    print("<-- Your Choices")
+    hammySelectedAbility = input()
     # If invalid input is given, the while loop is executed to get new input
     while(hammySelectedAbility not in hammyAbilities):
         print("Please enter one of the following abilities: ")
         for ability in hammyAbilities:
             print(ability)
-        hammySelectedAbility = input().lower()
+        hammySelectedAbility = input()
     # Return the selected ability
     return hammySelectedAbility
     
